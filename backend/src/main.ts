e58eba715c274ada68f9c4dbbe97f045c5bd0185
lanvalird <strazhognay@gmail.com>
2025-08-01 22:30:39 +0400

    backend: add CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin:
+        process.env.NODE_ENV === 'development'
+          ? true
+          : process.env.CORS_ALLOWED_ORIGINS?.split(',') || [],
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      allowedHeaders: 'Content-Type, Accept',
+    },
+  });
+
   await app.listen(process.env.PORT ?? 3000);
+
+  console.log(
+    `🚀 Server running on http://localhost:${process.env.PORT ?? 3000}`,
+  );
+  console.log(`🌍 CORS whitelist: ${process.env.CORS_ALLOWED_ORIGINS}`);
 }
+
 bootstrap();
