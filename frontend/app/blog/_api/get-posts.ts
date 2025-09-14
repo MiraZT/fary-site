9f52701ccc6f86dd11ad47677ac9cb06aebf2037
lanvalird <strazhognay@gmail.com>
2025-09-15 00:17:21 +0400

    frontend: update css file to as in-design Figma

--- frontend/app/blog/_api/get-posts.ts
+++ frontend/app/blog/_api/get-posts.ts
@@ -2,12 +2,20 @@ import type { Post } from "@/types";
 import { cache } from "react";
 
 export const getPosts = cache(async (): Promise<Post[]> => {
-  const data = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/posts`, {
-    cache: "force-cache",
-    next: {
-      revalidate: 21_600, // 6 hours, maybe
-    },
-  });
+  try {
+    const data = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/posts`, {
+      cache: "force-cache",
+      next: {
+        revalidate: 21_600, // 6 hours, maybe
+      },
+    });
 
-  return await data.json();
+    return await data.json();
+  } catch (e: unknown) {
+    if (e instanceof Error) {
+      console.error(e);
+    }
+
+    return [];
+  }
 });
